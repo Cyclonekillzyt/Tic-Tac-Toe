dc9af9d23581089138868356a8803cecaa059a12
Cyclonekillzyt <kendrickscyclone@gmail.com>
2025-01-27 20:11:13 +0000

    making progress

--- script.js
+++ script.js
@@ -1,7 +1,27 @@
 let board = document.querySelectorAll(".board")
 
+let producePlayer = (PlayerName, Marker) => {
+    let score = [];
+    const getPlayerName = () =>{return PlayerName}
+    const getMarker = () =>{return Marker};
+    const getScore = () =>{return score};
+    const addScore = (points) =>{score.push(points)};
+    return{
+        getPlayerName,getMarker,getScore,addScore
+    }
+}
+
+const player1 = producePlayer("Kojo", "X");
+const player2 = producePlayer("Kwame", "O");
+
+const updateGameBoard = (boardValue) => {
+    boardValue.textContent = player1.getMarker();
+}
+
 board.forEach(board => {
     board.addEventListener("click",() =>{
-        console.log(`${board.value}`)
+        updateGameBoard(board);
     })
 });
+
+
